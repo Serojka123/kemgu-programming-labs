562b0f6b0e2bcf3e7be76487decbf064d50a494b
Serojka123 <sersur42@yandex.ru>
2025-09-09 09:54:20 +0700

    upd

---
+++ lab1/homeWork2.cpp
@@ -0,0 +1,17 @@
+// Я выбрал 8 задание из списка
+
+#include <iostream>
+
+int main(){
+    int a;
+    int firsta;
+
+    std::cin >> a;
+
+    firsta = a;
+    a = a*a*a; //a^3
+    a = a*a; //a^6
+    a = a*firsta;
+    
+    std::cout << firsta << " в 7 степени = " << a << std::endl;
+}
