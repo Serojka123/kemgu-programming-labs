66cd25330eb27ae45e49b4e3fb988ac96a99ab71
Serojka123 <sersur42@yandex.ru>
2025-10-07 09:09:31 +0700

    upd

---
+++ lab7/homeWork1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+    int n = 0;
+    std::cout << "n = ";
+    std::cin >> n;
+
+    int x = 0;
+    std::cout << "x = ";
+    std::cin >> x; 
+
+    double sum = 0;
+    for (int k = 2; n >= k; k++){
+
+        sum += pow(-1,k)*pow(x-3, k)/k;
+
+        std::cout << sum << std::endl;
+    }
+}
