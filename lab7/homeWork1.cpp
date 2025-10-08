e43b3c909a38a0052121f5ec25c638d14b4685e5
Serojka123 <sersur42@yandex.ru>
2025-10-08 16:38:32 +0700

    upd

--- lab7/homeWork1.cpp
+++ lab7/homeWork1.cpp
@@ -9,12 +9,46 @@ int main(){
     std::cout << "x = ";
     std::cin >> x; 
 
+    int select = 0;
+    std::cout << "Выбрать способ:\n 1 - for\n 2 - while\n 3 - do while\n select = " ;
+    std::cin >> select; 
+    std::cout << "\n";
+
     double sum = 0;
-    for (int k = 2; n >= k; k++){
+    switch (select){
+        case 1:{
+            double sum = 0;
+            for (int k = 2; n >= k; k++){
 
-        sum += pow(-1,k)*pow(x-3, k)/k;
+                sum += pow(-1,k)*pow(x-3, k)/k;
 
-        
+                
+            }
+            std::cout << sum << std::endl;
+        }
+
+        case 2:{
+            double sum = 0;
+            int k = 2; 
+            while (n >= k){
+                sum += pow(-1,k)*pow(x-3, k)/k;
+                k++;
+                
+            }
+            std::cout << sum << std::endl;
+        }
+
+        case 3:{
+            double sum = 0;
+            int k = 2; 
+            do{
+                sum += pow(-1,k)*pow(x-3, k)/k;
+                k++;
+            }
+            while (n >= k);
+            std::cout << sum << std::endl;
+        }
     }
-    std::cout << sum << std::endl;
+    
+    
 }
