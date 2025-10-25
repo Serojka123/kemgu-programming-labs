015c1b6d32145625c9949cd3d15f92f4026cfc82
Ser0jka123 <sersur42@yandex.ru>
2025-10-25 13:37:42 +0700

    upd

---
+++ lab9/homeWork1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+
+int lenNum(int num){
+    int len = 0;
+    while (num>0){
+        len += 1;
+        num /= 10;
+    }
+    return len;
+}
+
+int main(){
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    float x = 0, y = 0, z = 0;
+    std::cout << "Введите 3 натуральных числа\n";
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "y = ";
+    std::cin >> y;
+    std::cout << "z = ";
+    std::cin >> z;
+
+    if ((x < 0 || fmod(x, 1.0) != 0.0 ) || 
+        (y < 0 || fmod(y, 1.0) != 0.0 ) || 
+        (z < 0 || fmod(z, 1.0) != 0.0 )
+    ){
+        std::cout << "Вы ввели не натуральные числа";
+        return 0;
+    }
+
+    if (lenNum(x) > lenNum(y)){
+        if (lenNum(x) > lenNum (z)){
+            std::cout << "Первое число имеет больше всего цифр\n";
+        }
+        else{
+            std::cout << "Третье число имеет больше всего цифр\n";
+        }
+    }
+    else{
+        if (lenNum(y) > lenNum(z)){
+            std::cout << "Второе число имеет больше всего цифр\n";
+        }
+        else{
+            std::cout << "Третье число имеет больше всего цифр\n";
+        }
+    }
+
+}
---
+++ lab9/homeWork2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+
+float P = 3.14159;
+
+float squareCircle(int R){
+    float square = P*pow(R,2);
+    return square;
+}
+
+float lenCircle(int R){
+    float len = 2*P*R;
+    return len;
+}
+
+
+void printTable( float a, float b, float c){
+    std::cout << "| " << a << " | S: " << b << " | L: " << c << " |" << std::endl;
+    std::cout << "-----" <<std::endl;
+
+
+}
+
+int main(){
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    std::cout << "n = ";
+    int n = 0;
+    std::cin >> n;
+
+    std::cout << "\n-----" <<std::endl;
+    for (int k = 1; k <= n; k++){
+        
+        printTable(k, squareCircle(k), lenCircle(k));
+
+    }
+    
+
+}
